32057a084fbfe5f83ea5571662a4a319dd1b431f
ycjing <ycjing@zju.edu.cn>
2021-02-01 16:27:53 +0800

    a

---
+++ models/dgl/binaryfunction.py
@@ -0,0 +1,19 @@
+from torch.autograd import Function
+import torch
+
+
+class BinaryQuantize(Function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        out = torch.sign(input)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[input.ge(1)] = 0
+        grad_input[input.le(-1)] = 0
+        return grad_input
+
